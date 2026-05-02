663e76bead54b07ae7f93c03359d5489da3ed8ae
Joao-Matheus-Amorim <202209304731@alunos.estacio.br>
2026-05-02 14:36:52 -0300

    Importa identidade premium nas páginas internas

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import './premium.css'
 import './membership.css'
 import './landing-mobile-fixes.css'
+import './premium-identity-pages.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
