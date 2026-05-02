9fa3f33735e899d960486331e86bbf0b10414150
Joao-Matheus-Amorim <202209304731@alunos.estacio.br>
2026-05-02 06:29:01 -0300

    feat: load membership design stylesheet

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import './premium.css'
+import './membership.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
